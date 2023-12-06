66cc4b834b05ffcad8032986cbc08725cfad3ed3
Siftab <siiffuuu@gmail.com>
2023-12-06 10:54:46 +0600

    first

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import PropTypes from "prop-types"
 import { useState } from 'react';
 import { createContext } from 'react';
+import {auth}   from '../Firebase.config'
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { useEffect } from 'react';
 export const AuthContext = createContext();
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
     const createUser=(email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
 
     }
+    const signIn=(email,password)=>{
+        return signInWithEmailAndPassword(auth,email,password)
+
+    }
+    const LogOut=()=>{
+       return signOut(auth);
+    }
+    useEffect(()=>{
+        const unSubscribe =onAuthStateChanged(auth,currentUser=>{
+            setUser(currentUser);
+        });
+        return ()=>unSubscribe();
+    },[])
 
 
 
- const authInfo={user}
+ const authInfo={user,createUser,signIn,LogOut}
 
     return (
         <AuthContext.Provider value={authInfo}>
@@ -20,3 +37,6 @@ const AuthProvider = ({children}) => {
 };
 
 export default AuthProvider;
+AuthProvider.propTypes={
+    children: PropTypes.object,
+}
